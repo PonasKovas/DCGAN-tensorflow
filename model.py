d1f2b3fec29e5bacfdf9615f16f44df2404c38e9
Mykolas Peteraitis <mykolas.peteraitis@gmail.com>
2020-06-20 16:49:15 +0300

    Update model.py

--- model.py
+++ model.py
@@ -24,7 +24,7 @@ class DCGAN(object):
   def __init__(self, sess, input_height=108, input_width=108, crop=True,
          batch_size=64, sample_num = 64, output_height=64, output_width=64,
          y_dim=None, z_dim=100, gf_dim=64, df_dim=64,
-         gfc_dim=1024, dfc_dim=1024, c_dim=4, dataset_name='default',
+         gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',
          max_to_keep=1,
          input_fname_pattern='*.jpg', checkpoint_dir='ckpts', sample_dir='samples', out_dir='./out', data_dir='./data'):
     """
